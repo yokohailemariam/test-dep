7d14a401b35eafd8b0132ad508b044d68c01c810
Yoko Hailemariam <Yokohailemariam@gmail.com>
2024-02-21 23:59:37 -0800

    refactor

--- app/components/anim.tsx
+++ app/components/anim.tsx
@@ -1,11 +1,11 @@
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const ImageSlide = ({ imageUrl }: { imageUrl: string }) => {
   const variants = {
     hidden: {
       x: "20vw",
       scale: 0.2,
-      rotate: 45
+      rotate: 45,
     },
     visible: {
       x: 0,
@@ -14,13 +14,13 @@ const ImageSlide = ({ imageUrl }: { imageUrl: string }) => {
       transition: {
         delay: 0,
         duration: 1.5,
-        ease: "easeInOut"
-      }
-    }
-  }
+        ease: "easeInOut",
+      },
+    },
+  };
 
   return (
-    <div className=" drop-shadow-2xl">
+    <>
       <motion.img
         src={imageUrl}
         variants={variants}
@@ -30,8 +30,8 @@ const ImageSlide = ({ imageUrl }: { imageUrl: string }) => {
         alt="leza awards logo"
       />
       <div className="shadow-2xl w-44 mt-10"></div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default ImageSlide
+export default ImageSlide;
